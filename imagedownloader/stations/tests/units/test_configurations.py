4cc0d363b281854af371f3df081ccea019597a24
eloy <eloy.colell@gmail.com>
2013-12-08 00:12:45 -0300

    New test for Configuration from stations.

--- imagedownloader/stations/tests/units/test_configurations.py
+++ imagedownloader/stations/tests/units/test_configurations.py
@@ -32,9 +32,22 @@ class TestConfigurations(TestCase):
 		for c in are_active:
 			c.go_inactive()
 		self.assertEqual(len(Configuration.actives()), 0)
-		
-		#self.seq.sort()
-		#self.assertEqual(self.seq, range(10))
+
+	def test_get_backup_filename(self):
+		# check if the backup-filename uses the
+		# stations/backup/ folder.
+		before = datetime.utcnow().replace(microsecond=0)
+		filename = self.configuration.get_backup_filename("file.xls")
+		after = datetime.utcnow().replace(microsecond=0)
+		parts = filename.split(".")
+		self.assertEqual(parts[0][:-14], "stations/backup/")
+		# then test if the datetime added at the begining of the name use the UTC
+		# clock (and check that is between 2 datetime captures).
+		dt = datetime.strptime(parts[0][-14:],"%Y%m%d%H%M%S")
+		self.assertTrue(before <= dt <= after)
+		# finish, test if the original name is at the end of the generated
+		# backup-filename. 
+		self.assertEqual(filename[-8:], "file.xls")
 
 	"""
         # should raise an exception for an immutable sequence
