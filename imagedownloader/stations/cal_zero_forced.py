1a7b7e02c46cdf96237e8d9137eb4870509149cf
eloy <eloy.colell@gmail.com>
2014-01-13 19:31:09 -0300

    landscape.io: Import only necessary and ident correctly in stations/cal_zero_forced.py (issue #11).

--- imagedownloader/stations/cal_zero_forced.py
+++ imagedownloader/stations/cal_zero_forced.py
@@ -1,7 +1,5 @@
 import sys
 from pylab import show, plot, figure, subplot, mean, genfromtxt, corrcoef, legend
-from scipy import stats
-from scipy.optimize import curve_fit
 
 x = genfromtxt(fname=sys.argv[1], skip_header = 1, usecols = 0 )
 xi = x
@@ -10,31 +8,20 @@ for col in xrange(1,7):
 	figure(1)
 	subplot(2,3,col )
 	y =  genfromtxt(fname=sys.argv[1], skip_header = 1, usecols = col )
-	yi = y	
-	   
-
+	yi = y
 	#slope calculated for b = 0 in y = ax + b
 	a = mean(xi*yi)/mean(xi**2)
-
-
 	corrr = corrcoef(yi, a*xi) 
 	#print 'corrr ', corrr[1][0]
 	print 'slope '+str(col), a
-	
 	print corrcoef(yi, a*xi)[1][0] 
 	#def func(xi, a):
 	#	return a*xi
-
 	#popt, pcov = curve_fit(func, xi, yi)	
-
 	#print 'popt', popt
 	#print 'pcov', pcov 
-
 	line = a*xi
-
 	plot(xi,line,'r-',xi,yi,'o', label = a)
 	legend()
 	#legend(['m ', 'r**2 '],[a, corrr])
-
 show()
-
