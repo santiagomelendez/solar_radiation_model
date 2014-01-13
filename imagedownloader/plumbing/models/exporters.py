b4486c54e6c1353dae744b3dfbaf97830aceb336
eloy <eloy.colell@gmail.com>
2014-01-13 20:51:16 -0300

    landscape.io: Remove unused variables in plumbing/models/exporters.py (issue #11).

--- imagedownloader/plumbing/models/exporters.py
+++ imagedownloader/plumbing/models/exporters.py
@@ -15,8 +15,8 @@ class Compact(Process):
 
 	def do(self, stream):
 		filename = "%spkg.%s.nc" % (self.resultant_stream.root_path,stream.tags.make_filename())
-		file = self.do_file(filename,stream)
-		fs = FileStatus(file=file,stream=self.resultant_stream)
+		f = self.do_file(filename,stream)
+		fs = FileStatus(file=f,stream=self.resultant_stream)
 		fs.save()
 		return self.resultant_stream
 
@@ -25,10 +25,9 @@ class Compact(Process):
 
 	def do_file(self, filename, stream):
 		# create compact file and initialize basic settings
-		begin_time = self.getdatetimenow()
 		root, is_new = nc.open(filename)
 		if is_new:
-			sample, n = nc.open(stream.files.all()[0].file.completepath())
+			sample = nc.open(stream.files.all()[0].file.completepath())[0]
 			shape = sample.variables['data'].shape
 			nc.getdim(root,'northing', shape[1])
 			nc.getdim(root,'easting', shape[2])
@@ -47,16 +46,14 @@ class Compact(Process):
 		return f
 
 	def do_var(self, root, var_name, stream):
-		count = 0
 		file_statuses = stream.sorted_files()
 		shape = nc.getvar(root,'lat').shape
 		for fs in file_statuses:
 			# join the distributed content
-			ch = fs.file.channel()
 			v_ch   = nc.getvar(root,var_name, 'f4', ('timing','northing','easting',), 4)
 			v_ch_t = nc.getvar(root,var_name + '_time', 'f4', ('timing',))
 			try:
-				rootimg, n = nc.open(fs.file.completepath())
+				rootimg = nc.open(fs.file.completepath())[0]
 				data = (nc.getvar(rootimg, 'data'))[:]
 				# Force all the channels to the same shape
 				if not (data.shape[1:3] == shape):
@@ -68,5 +65,5 @@ class Compact(Process):
 					v_ch_t[index] = calendar.timegm(fs.file.datetime().utctimetuple())
 				nc.close(rootimg)
 				nc.sync(root)
-			except RuntimeError, e:
+			except RuntimeError:
 				print fs.file.completepath()
