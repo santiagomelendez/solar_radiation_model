0379b42f1e9117e95773638ce586d9db22da59d5
eloy <eloy.colell@gmail.com>
2014-02-04 18:44:34 -0300

    Complete test unit statements for Process (issue #10 and #13).

--- imagedownloader/plumbing/tests/units/test_processes.py
+++ imagedownloader/plumbing/tests/units/test_processes.py
@@ -11,12 +11,18 @@ class TestProcesses(TestCase):
 	def setUp(self):
 		self.process = Process.objects.get(name='year.Mmonth')
 		self.other_process = Process.objects.get(pk=6)
+		self.last_process = Process(name='Testing', description='This is a fake process only for testing.')
 		self.stream = Stream(root_path="/var/service/data/GVAR_IMG/argentina/")
 
 	def test_serialization(self):
 		# check if the __str__ method is defined to return the class name with the name parameter.
-		self.assertEquals(str(self.process), 'CollectTimed [year.Mmonth]')
-		self.assertEquals(str(self.other_process), "FilterSolarElevation [Filter night's images]")
+		process = u'CollectTimed [year.Mmonth]'
+		other_process = u"FilterSolarElevation [Filter night's images]"
+		self.assertEquals(str(self.process), str(process))
+		self.assertEquals(str(self.other_process), str(other_process))
+		# check if the __unicode__ method is defined to return the class name with the name parameter.
+		self.assertEquals(unicode(self.process), process)
+		self.assertEquals(unicode(self.other_process), other_process)
 
 	def test_mark_with_tags(self):
 		# check if the mark_with_tags method in the Process class don't
@@ -24,4 +30,5 @@ class TestProcesses(TestCase):
 		self.process.mark_with_tags(self.stream)
 		self.assertTrue(self.stream.empty())
 		self.other_process.mark_with_tags(self.stream)
+		self.last_process.mark_with_tags(self.stream)
 		self.assertTrue(self.stream.empty())
