1e5cbd26bfc2e434935378869be9effd3070e43e
eloy <eloy.colell@gmail.com>
2014-03-02 11:22:26 -0300

    Instead of define the plumbing api it extend the factopy api (issue #17).

--- imagedownloader/plumbing/admin.py
+++ imagedownloader/plumbing/admin.py
@@ -1,61 +1,11 @@
 from django.contrib import admin
 from polymorphic.admin import PolymorphicParentModelAdmin, PolymorphicChildModelAdmin, PolymorphicChildModelFilter
-from plumbing.models import TagManager, Stream, MaterialStatus, Material, Process, ProcessOrder, ComplexProcess, Program, File, CollectTimed, CollectChannel, FilterTimed, FilterChannel, FilterSolarElevation, AppendCountToRadiationCoefficient, Compact, SyncImporter
+from plumbing.models import Program, File, CollectTimed, CollectChannel, FilterTimed, FilterChannel, FilterSolarElevation, AppendCountToRadiationCoefficient, Compact, SyncImporter
 from django.forms import ModelForm
+from factopy.admin import MaterialAdmin, ProcessAdmin, MaterialChildAdmin, ProcessChildAdmin, ComplexProcessChildAdmin
 
-
-class TagManagerAdmin(admin.ModelAdmin):
-	list_display = ['tag_string']
-
-
-class StreamAdmin(admin.ModelAdmin):
-	list_display = ['root_path', 'created', 'modified']
-
-
-class MaterialChildAdmin(PolymorphicChildModelAdmin):
-	base_model = Material
-
-
-class MaterialAdmin(PolymorphicParentModelAdmin):
-	base_model = Material
-	list_filter = (PolymorphicChildModelFilter,)
-	child_models = (
-		(File, MaterialChildAdmin),
-	)
-
-
-class MaterialStatusAdmin(admin.ModelAdmin):
-	list_display = ['stream', 'material']
-
-
-class ProcessChildAdmin(PolymorphicChildModelAdmin):
-	base_model = Process
-
-
-class ProcessInlineForm(ModelForm,object):
-	class Meta(object):
-			model = ProcessOrder
-			fields = ['position','process']
-
-
-class ProcessOrderInline(admin.TabularInline):
-	form = ProcessInlineForm
-	model = ProcessOrder
-	fk_name = 'complex_process'
-	extra = 0 # how many rows to show
-	ordering = ["position",]
-
-
-class ComplexProcessChildAdmin(ProcessChildAdmin):
-	list_display = [ 'name', 'description']
-	inlines = [ProcessOrderInline]
-	search_fields = ['name', 'description', ]
-
-
-class ProcessAdmin(PolymorphicParentModelAdmin):
-	base_model = Process
-	list_filter = (PolymorphicChildModelFilter,)
-	child_models = (
+MaterialAdmin.child_models += ((File, MaterialChildAdmin),)
+ProcessAdmin.child_models += (
 		(CollectTimed, ProcessChildAdmin),
 		(CollectChannel, ProcessChildAdmin),
 		(FilterTimed, ProcessChildAdmin),
@@ -63,14 +13,6 @@ class ProcessAdmin(PolymorphicParentModelAdmin):
 		(FilterSolarElevation, ProcessChildAdmin),
 		(AppendCountToRadiationCoefficient, ProcessChildAdmin),
 		(Compact, ProcessChildAdmin),
-		(ComplexProcess, ComplexProcessChildAdmin),
 		(Program, ComplexProcessChildAdmin),
 		(SyncImporter, ProcessChildAdmin),
 	)
-
-
-admin.site.register(TagManager, TagManagerAdmin)
-admin.site.register(Stream, StreamAdmin)
-admin.site.register(Material, MaterialAdmin)
-admin.site.register(MaterialStatus, MaterialStatusAdmin)
-admin.site.register(Process, ProcessAdmin)
--- imagedownloader/plumbing/urls.py
+++ imagedownloader/plumbing/urls.py
@@ -1,16 +1,6 @@
 from django.conf.urls import patterns, include, url
-from tastypie.api import Api
-from plumbing.api import StreamResource, MaterialResource, MaterialStatusResource, ProcessResource
-
-
-api_v1 = Api(api_name='v1')
-api_v1.register(StreamResource())
-api_v1.register(MaterialResource())
-api_v1.register(MaterialStatusResource())
-api_v1.register(ProcessResource())
 
 urlpatterns = patterns('',
-	url(r'^api/', include(api_v1.urls)),
 	url(r'^program/$', 'plumbing.views.index'),
 	url(r'^program/execute/(?P<program_id>\d+)/$', 'plumbing.views.execute'),
 	url(r'^program/status$', 'plumbing.views.status'),
