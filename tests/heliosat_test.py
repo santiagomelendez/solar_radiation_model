b058c1116d0c4864c4f362857f0584dbafda519c
eloy <eloy.colell@gmail.com>
2015-02-26 11:28:53 -0300

    It append a time estimation for the whole Argentinian country with a month window.

--- tests/heliosat_test.py
+++ tests/heliosat_test.py
@@ -2,7 +2,7 @@ import unittest
 from models.heliosat import main
 import numpy as np
 from netcdf import netcdf as nc
-import datetime
+from datetime import datetime
 import os
 import glob
 
@@ -17,7 +17,17 @@ class TestHeliosat(unittest.TestCase):
         os.system('rm -rf mock_data')
 
     def test_main(self):
+        begin = datetime.now()
         main.workwith('mock_data/goes13.2015.*.BAND_01.nc')
+        end = datetime.now()
+        elapsed = (end - begin).total_seconds()
+        first, last = min(self.files), max(self.files)
+        to_dt = main.to_datetime
+        processed = (to_dt(last) - to_dt(first)).total_seconds()
+        processed_days = processed / 3600. / 24
+        scale_shapes = (2245. / 86) * (3515. / 180) * (30. / processed_days)
+        estimated = elapsed * scale_shapes / 3600.
+        print "Scaling total time to %.2f hours." % estimated
 
 
 if __name__ == '__main__':
