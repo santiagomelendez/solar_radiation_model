a52790ac9945d1a273abad2910d1ee6794af3700
eloy <eloy.colell@gmail.com>
2015-03-20 19:30:59 -0300

    It updated the goesdownloader to a new version.

--- models/runner.py
+++ models/runner.py
@@ -2,12 +2,15 @@ import heliosat
 from goesdownloader import instrument as goes
 import os
 import requests
+from datetime import datetime, timedelta
 
 
 def run():
-    print os.environ
+    diff = lambda dt, h: (dt - timedelta(hours=h))
+    decimal = lambda dt, h: diff(dt, h).hour + diff(dt, h).minute / 60. + diff(dt, h).second / 3600.
+    should_download = lambda dt: decimal(dt, 4) >= 5 and decimal(dt, 4) <= 20
     filenames = goes.download('noaa.gvarim', 'noaaadmin', 'data_argentina',
-                              name='Argentina')
+                              name='Argentina', datetime_filter=should_download)
     print filenames
     # if filenames:
     #     heliosat.workwith('data/goes13.*.BAND_01.nc')
