3d8074601e651479a0112fe1d61ec47a82a0766c
Eloy Adonis Colell <eloy.colell@gmail.com>
2015-05-09 17:01:38 -0300

    It accept a max difference of 2.5 in each pixel (issue #70).

--- tests/heliosat_test.py
+++ tests/heliosat_test.py
@@ -10,7 +10,8 @@ import glob
 class TestHeliosat(unittest.TestCase):
 
     def setUp(self):
-        os.system('rm -rf static.nc temporal_cache products/estimated')
+        # os.system('rm -rf static.nc temporal_cache products')
+        os.system('rm -rf temporal_cache products/estimated')
         os.system('cp -rf data mock_data')
         self.files = glob.glob('mock_data/goes13.*.BAND_01.nc')
 
@@ -22,8 +23,7 @@ class TestHeliosat(unittest.TestCase):
             with nc.loader('products/estimated/*.nc') as new_root:
                 valid = nc.getvar(old_root, 'globalradiation')
                 calculated = nc.getvar(new_root, 'globalradiation')
-                np.testing.assert_array_almost_equal(calculated[:],
-                                                     valid[:], 0)
+                self.assertTrue((np.abs(calculated[:] - valid[:]) < 2.5).all())
 
     def test_main(self):
         begin = datetime.now()
