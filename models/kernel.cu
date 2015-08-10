5a6e515ec5542e32582adb32d6b51081a792ad62
Agustin Marrone <agustinhmarrone@gmail.com>
2015-08-10 12:35:18 -0300

    It updates the kernel indexes

--- models/kernel.cu
+++ models/kernel.cu
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 #define GREENWICH_LON (0.0f)
-#define PI (3.141592653589793f)
+#define PI (3.1415927f)
 #define DEG2RAD (PI / 180.0f)
 #define RAD2DEG (180.0f / PI)
 
 #define i_dt (threadIdx.z)
-#define i_dxy (blockIdx.x + (blockIdx.y * gridDim.x))
-#define i_dxyt (i_dxy + gridDim.x * gridDim.y * i_dt)
+#define i_dxy (threadIdx.x \
+               + (blockIdx.x * blockDim.x) \
+               + ((threadIdx.y + (blockIdx.y * blockDim.y)) * (gridDim.x * blockDim.x))) 
+#define i_dxyt (i_dxy + (gridDim.x * blockDim.x) * (gridDim.y * blockDim.y) * i_dt)
 
 
 __device__ void getdeclination(float *declination, float *gamma)
@@ -187,7 +189,6 @@ float diffuseirradiance)
 #define rpol 6356.5838f
 #define req  6378.1690f
 #define h    42166.55637f
-//define h    42164.0f
 
 __device__ float getsatellitalzenithangle(float *lat,
 float *lon, float *sub_lon)
