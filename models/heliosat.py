94e8536468d54370db696a032a6cd6a03953412e
Agustin Marrone <agustinhmarrone@gmail.com>
2015-08-18 22:36:18 -0300

    Improve merge.

--- models/heliosat.py
+++ models/heliosat.py
@@ -62,26 +62,15 @@ class Heliosat2(object):
     def update_temporalcache(self, loader, cache):
         logging.info("Updating temporal cache... ")
         self.strategy = self.strategy_type(self, loader, cache)
-        start = datetime.now()
         self.strategy.update_temporalcache(loader, cache)
-        diff = datetime.now() - start
-        print "\n --- UPDATE TEMPORAL CACHE TIME -> " + str(diff) + "\n"
 
     def estimate_globalradiation(self, loader, cache):
         # There is nothing to do, if there isn't new cache and strategy setted.
-<<<<<<< Updated upstream
         if hasattr(self, 'strategy'):
             logging.info("Obtaining the global radiation... ")
             output = OutputCache(self)
             self.strategy.estimate_globalradiation(loader, cache, output)
             output.dump()
-=======
-        #if hasattr(self, 'strategy'):
-        #    show("Obtaining the global radiation... ")
-        #    output = OutputCache(self)
-        #    self.strategy.estimate_globalradiation(loader, cache, output)
-        #    output.dump()
->>>>>>> Stashed changes
         cache.dump()
 
     def run_with(self, loader):
@@ -174,51 +163,8 @@ class OutputCache(AlgorithmCache):
         return '%s/%s' % (self.output_path, short(filename, None, None))
 
 
-<<<<<<< Updated upstream
 def run(**config):
-        loader = Loader(config['data'], tile_cut=config['tile_cut'])
-        algorithm = Heliosat2(config, geo.strategy)
-=======
-def filter_wrong_sized_files(files):
-    size = lambda f: os.stat(f).st_size
-    sizes = defaultdict(int)
-    for f in files:
-        sizes[size(f)] += 1
-    more_freq = max(sizes.values())
-    right_size = filter(lambda (s, freq): freq == more_freq, sizes.items())[0][0]
-    return filter(lambda f: size(f) == right_size, files)
-
-
-def filter_filenames(filename):
-    files = glob.glob(filename) if isinstance(filename, str) else filename
-    if not files:
-        return []
-    last_dt = to_datetime(max(files))
-    a_month_ago = (last_dt - timedelta(days=30)).date()
-    gmt = pytz.timezone('GMT')
-    local = pytz.timezone('America/Argentina/Buenos_Aires')
-    localize = lambda dt: (gmt.localize(dt)).astimezone(local)
-    in_the_last_month = lambda f: to_datetime(f).date() >= a_month_ago
-    files = filter(in_the_last_month, files)
-    files = filter_wrong_sized_files(files)
-    daylight = lambda dt: localize(dt).hour >= 6 and localize(dt).hour <= 20
-    files = filter(lambda f: daylight(to_datetime(f)), files)
-    return files
-
-
-def workwith(filename="data/goes13.*.BAND_01.nc", maxfiles=0):
-    filenames = filter_filenames(filename)
-    if filenames:
-        if maxfiles:
-            filenames = filenames[:maxfiles]
-        months = list(set(pmap(lambda dt: '%i/%i' % (dt.month, dt.year),
-                               pmap(to_datetime, filenames))))
-        show("=======================")
-        show("Months: ", months)
-        show("Dataset: ", len(filenames), " files.")
-        show("-----------------------\n")
-        loader = Loader(filenames)
-        algorithm = Heliosat2(filenames, geo.strategy)
->>>>>>> Stashed changes
-        algorithm.run_with(loader)
-        loader.dump()
+    loader = Loader(config['data'], tile_cut=config['tile_cut'])
+    algorithm = Heliosat2(config, geo.strategy)
+    algorithm.run_with(loader)
+    loader.dump()
