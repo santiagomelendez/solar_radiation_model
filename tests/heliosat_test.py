b33bcffd39ab59dd1fd269148c1a451fc2835481
eloy <eloy.colell@gmail.com>
2015-12-07 00:12:01 -0300

    Removed temporal_cache related lines.

--- tests/heliosat_test.py
+++ tests/heliosat_test.py
@@ -4,15 +4,12 @@ from datetime import datetime
 from models import JobDescription
 import os
 import glob
-import tempfile
 
 
 class TestHeliosat(unittest.TestCase):
 
     def setUp(self):
-        tmp = tempfile.gettempdir() + '/temporal_cache'
-        print tmp
-        os.system('rm -rf static.nc product %s' % tmp)
+        os.system('rm -rf static.nc product')
         os.system('cp -rf data mock_data')
         self.files = glob.glob('mock_data/goes13.*.BAND_01.nc')[:-1]
         self.tile_cut = {
